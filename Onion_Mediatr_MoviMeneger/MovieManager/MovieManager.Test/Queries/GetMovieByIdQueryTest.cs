b73e548e2dc6558d2e12cbbdb0a45a7045f2fb72
Dmytro Sharshakov <113136582+Picmaster911@users.noreply.github.com>
2024-04-13 09:26:38 +0300

    Test for Onion

---
+++ Onion_Mediatr_MoviMeneger/MovieManager/MovieManager.Test/Queries/GetMovieByIdQueryTest.cs
@@ -0,0 +1,51 @@
+﻿using Application.ProductFeatures.Queires;
+using AutoFixture;
+using Domain.Entities;
+using MovieManager.Tests;
+using Persistence.Context;
+using static Application.ProductFeatures.Queires.GetMovieByIdQuery;
+
+namespace MovieManager.Service.Tests.Queries
+{
+    public class GetMoviesByIdQueryTests
+    {
+        private readonly DbContextDecorator<MovieManagerContext> _dbContext;
+
+        public GetMoviesByIdQueryTests()
+        {
+            var options = Utilities.CreateInMemoryDbOptions<MovieManagerContext>();
+
+            _dbContext = new DbContextDecorator<MovieManagerContext>(options);
+        }
+
+        [Test]
+        public void DataSet_ReturnsCorrectRows()
+        {
+            var fixture = new Fixture();
+            var movie1 = fixture.Build<Movie>().With(x => x.MovieId, 1).Create();
+            var movie2 = fixture.Build<Movie>().With(x => x.MovieId, 2).Create();
+            _dbContext.AddAndSaveRange(new List<Movie> { movie1, movie2 });
+
+            _dbContext.Assert(async context => {
+                // Arrange
+                var sut = CreateSut(context);
+
+                // Act
+                var results = await sut.Handle(new GetMovieByIdQuery() { MovieId = 2 }); ;
+
+                // Assert
+                Assert.That(results, Is.Not.Null);
+
+                Assert.Multiple(() =>
+                {
+                    Assert.That(results.MovieId, Is.EqualTo(movie2.MovieId));
+                    Assert.That(results.Title, Is.EqualTo(movie2.Title));
+                    Assert.That(results.Description, Is.EqualTo(movie2.Description));
+                    Assert.That(results.ReleaseDate, Is.EqualTo(movie2.ReleaseDate));
+                });
+            });
+        }
+
+        private static GetMovieByIdQueryHandler CreateSut(MovieManagerContext context) => new(context);
+    }
+}
---
+++ Onion_Mediatr_MoviMeneger/MovieManager/MovieManager.Test/Queries/GetSessionsQueryTest.cs
@@ -0,0 +1,55 @@
+﻿using Application.ProductFeatures.Queires;
+using AutoFixture;
+using Domain.Entities;
+using MovieManager.Tests;
+using Persistence.Context;
+using static Application.ProductFeatures.Queires.GetMoviesQuery;
+using static Application.ProductFeatures.Queires.GetSessionsQuery;
+
+
+namespace MovieManager.Service.Tests.Queries
+{
+    public class GetMoviesSesionTests
+    {
+        private readonly DbContextDecorator<MovieManagerContext> _dbContext;
+
+        public GetMoviesSesionTests()
+        {
+            var options = Utilities.CreateInMemoryDbOptions<MovieManagerContext>();
+
+            _dbContext = new DbContextDecorator<MovieManagerContext>(options);
+        }
+
+        [Test]
+        public void DataSet_ReturnsCorrectRows()
+        {
+            var fixture = new Fixture();
+            var session1 = fixture.Build<Session>().With(x => x.SessionId, 1).Create();
+            var session2 = fixture.Build<Session>().With(x => x.SessionId, 2).Create();
+            _dbContext.AddAndSaveRange(new List<Session> { session1, session2 });
+
+            _dbContext.Assert(async context => {
+                // Arrange
+                var sut = CreateSut(context);
+
+                // Act
+                var results = await sut.Handle(new GetSessionsQuery());
+
+                // Assert
+                Assert.That(results, Is.Not.Null);
+                Assert.That(results.Count, Is.EqualTo(2));
+
+                var firstMovie = results.Last();
+                Assert.Multiple(() =>
+                {
+                    Assert.That(firstMovie.SessionId, Is.EqualTo(session1.SessionId));
+                    Assert.That(firstMovie.MovieId, Is.EqualTo(session1.MovieId));
+                    Assert.That(firstMovie.RoomName, Is.EqualTo(session1.RoomName));
+                    Assert.That(firstMovie.StartDateTime, Is.EqualTo(session1.StartDateTime));
+                });
+            });
+        }
+
+        private static GetSessionsQueryHandler CreateSut(MovieManagerContext context) => new(context);
+    }
+}
